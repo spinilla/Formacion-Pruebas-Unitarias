71f31d61440d35f35f8d08d603054c058cbcb7f7
SERGIO PINILLA GARCIA <spinillag@viewnext.com>
2018-02-10 13:45:56 +0100

    Hamcrest - Proyecto Eclipse - Ejercicio

---
+++ HamcrestTutorial/src/main/java/example/calculator/CalculatorEngine.java
@@ -0,0 +1,73 @@
+package example.calculator;
+public class CalculatorEngine {
+
+    private enum Operator {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE
+    }
+
+    double currentTotal;
+
+    public String getTotalString() {
+        return currentTotal % 1.0 == 0
+                ? Integer.toString((int) currentTotal)
+                : String.valueOf(currentTotal);
+    }
+
+    public void equal(String number) {
+        currentTotal = Double.parseDouble(number);
+    }
+
+    public void add(String number) {
+        convertToDouble(number, Operator.ADD);
+    }
+
+    public void subtract(String number) {
+        convertToDouble(number, Operator.SUBTRACT);
+    }
+
+    public void multiply(String number) {
+        convertToDouble(number, Operator.MULTIPLY);
+    }
+
+    public void divide(String number) {
+        convertToDouble(number, Operator.DIVIDE);
+    }
+
+    private void convertToDouble(String number, Operator operator) {
+        double dblNumber = Double.parseDouble(number);
+        switch (operator) {
+            case ADD:
+                add(dblNumber);
+                break;
+            case SUBTRACT:
+                subtract(dblNumber);
+                break;
+            case MULTIPLY:
+                multiply(dblNumber);
+                break;
+            case DIVIDE:
+                divide(dblNumber);
+                break;
+            default:
+                throw new AssertionError(operator.name());
+        }
+    }
+
+    void add(double number) {
+        currentTotal += number % 1.0 == 0 ? (int) number : number;
+    }
+
+    void subtract(double number) {
+        currentTotal -= number % 1.0 == 0 ? (int) number : number;
+    }
+
+    void multiply(double number) {
+        currentTotal *= number % 1.0 == 0 ? (int) number : number;
+    }
+
+    void divide(double number) {
+        currentTotal /= number % 1.0 == 0 ? (int) number : number;
+    }
+
+}
+
---
+++ HamcrestTutorial/src/test/java/com/insags/hamcresttutorial/AllTests.java
@@ -0,0 +1,11 @@
+package com.insags.hamcresttutorial;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SolucionesHamcrestTutorialTest.class, HamcrestTutorialTest.class })
+public class AllTests {
+
+}
