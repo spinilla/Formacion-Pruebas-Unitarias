6fee6f8c36af766ec66bc1a969053758ac0861ab
SERGIO PINILLA GARCIA <spinilla@gmail.com>
2018-02-26 23:27:22 +0100

    Added project Spring Tutorial Mongo Test with BDD embedded with de.flapdoodle.embed.mongo

---
+++ SpringTutorialMongoTest/src/main/java/com/viewnext/formacion2018/pruebasunitarias/mongo/MongodbExampleApplication.java
@@ -0,0 +1,25 @@
+package com.viewnext.formacion2018.pruebasunitarias.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * SpringTutorialMongoTest
+ * com.viewnext.formacion2018.pruebasunitarias.mongo
+ * 
+ * MongodbExampleApplication
+ * 
+ * @author Sergio Pinilla Garcia (Viewnext/IBM)
+ * @version 0.1
+ * @since 26 feb. 2018
+ *
+ */
+@SpringBootApplication
+public class MongodbExampleApplication{
+
+  public static void main(String[] args) {
+    SpringApplication.run(MongodbExampleApplication.class, args);
+  }
+
+}
